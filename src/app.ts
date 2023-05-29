104eef846c08105a46f4e55c3ced818e7fa2e86e
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-05-29 11:57:25 +0600

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express, { Application, Request, Response } from 'express'
-const app: Application = express()
-import cors from 'cors'
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+import cors from "cors";
 
-app.use(cors())
+app.use(cors());
 
 // parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to your Dream University Dashboard !')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to your Dream University Dashboard !");
+});
 
-export default app
+export default app;
