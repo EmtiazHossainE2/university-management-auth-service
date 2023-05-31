21c9f0411c32ec8ee4de6abd64b9799bfe0bc37a
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-05-31 15:55:30 +0600

    user module done with create new user

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
 import cors from "cors";
+import usersRouter from "./app/modules/users/users.route";
 
 app.use(cors());
 
@@ -8,8 +9,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Application Routes
+app.use("/api/v1/users/", usersRouter);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to your Dream University Dashboard !");
 });
+
+// Test Route
+// app.get("/test", async (req: Request, res: Response) => {
+//   await usersService.createUser({
+//     id: '310',
+//     role: 'admin',
+//     password:"yoyobaba"
+//   })
+//   res.send("Welcome to test route");
+// });
 
 export default app;
